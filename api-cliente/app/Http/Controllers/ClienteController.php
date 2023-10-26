e23b859fc84d740c6289873a02b8bca41d9f3cf4
Kevin Aranda <kevinaranda042000@gmail.com>
2023-10-26 09:49:16 -0300

    Revert "reformulación de las respuestas de json"
    This reverts commit 3fc0dca2500c9e3f55a85e39b0010e3b9f2c454e.

--- api-cliente/app/Http/Controllers/ClienteController.php
+++ api-cliente/app/Http/Controllers/ClienteController.php
@@ -6,124 +6,64 @@ use App\Models\Cliente;
 use Illuminate\Http\Request;
 use App\Http\Requests\ClienteRequest;
 use Carbon\Carbon;
-use Illuminate\Http\Response; // Importar la clase Response para utilizar códigos HTTP estándar
-
 
 class ClienteController extends Controller
 {
     public function index()
     {
-        // Obtener todos los clientes
-        $clientes = Cliente::all();
-
-        // Si no hay clientes, devuelve un error 404
-        if ($clientes->isEmpty()) {
-            return response()->json([
-                'status' => 'error',
-                'message' => 'No se encontraron clientes.',
-                'data' => []
-            ], 404); // Código HTTP 404 Not Found
-        }
-    
-        // Si hay clientes, devuélvelos con un código 200
-        return response()->json([
-            'status' => 'success',
-            'message' => 'Clientes obtenidos con éxito.',
-            'data' => $clientes
-        ], 200); // Código HTTP 200 OK
+        return Cliente::all();
     }
 
     public function store(ClienteRequest $request)
     {
-        try {
-            // Crea una nueva instancia del modelo Cliente y llena los campos
-            $cliente = new Cliente;
-            // (...)
-            $cliente->save();
+        $cliente = new Cliente;
+        $cliente->name = $request->name;
+        $cliente->surnames = $request->surnames;
+        $cliente->phone_number = $request->phone_number;
+        $cliente->email = $request->email;
+        $cliente->birthdate = Carbon::createFromFormat('d/m/Y', $request->birthdate)->toDateString();
+        $cliente->address = $request->address;
+        $cliente->country = $request->country;
+        $cliente->province = $request->province;
+        $cliente->city = $request->city;
+        $cliente->postal_code = $request->postal_code;
 
-            // Si todo sale bien, devuelve el cliente creado con un código 201
-            return response()->json([
-                'status' => 'success',
-                'message' => 'Cliente creado con éxito.',
-                'data' => $cliente
-            ], 201); // Código HTTP 201 Created
-    
-        } catch (\Exception $e) {
-            // Si algo sale mal, captura la excepción y devuelve un mensaje de error con código 500
-            return response()->json([
-                'status' => 'error',
-                'message' => 'Hubo un error al crear el cliente.',
-                'error' => $e->getMessage()
-            ], 500); // Código HTTP 500 Internal Server Error
-        }
+        $cliente->save();
+
+        return $cliente;
     }
 
-    
     public function show(Cliente $cliente)
     {
-        // Inyección implícita de modelos. Si no encuentra el cliente, Laravel manejará automáticamente un error 404
-        // Si encuentra el cliente, lo devuelve con un código 200
-        return response()->json([
-            'status' => 'success',
-            'message' => 'Cliente obtenido con éxito.',
-            'data' => $cliente
-        ], 200); // Código HTTP 200 OK
+        return $cliente;
     }
 
     public function update(ClienteRequest $request, Cliente $cliente)
     {
-        try {
-            // Actualiza los campos del cliente
-            // (...)
-            $cliente->update();
+        $cliente->name = $request->name;
+        $cliente->surnames = $request->surnames;
+        $cliente->phone_number = $request->phone_number;
+        $cliente->email = $request->email;
+        $cliente->birthdate = Carbon::createFromFormat('d/m/Y', $request->birthdate)->toDateString();
+        $cliente->address = $request->address;
+        $cliente->country = $request->country;
+        $cliente->province = $request->province;
+        $cliente->city = $request->city;
+        $cliente->postal_code = $request->postal_code;
+        $cliente->update();
 
-            // Si todo sale bien, devuelve el cliente actualizado con un código 200
-            return response()->json([
-                'status' => 'success',
-                'message' => 'Cliente actualizado con éxito.',
-                'data' => $cliente
-            ], 200); // Código HTTP 200 OK
-            
-        } catch (\Exception $e) {
-            // Si algo sale mal, captura la excepción y devuelve un mensaje de error con código 500
-            return response()->json([
-                'status' => 'error',
-                'message' => 'Hubo un error al actualizar el cliente.',
-                'error' => $e->getMessage()
-            ], 500); // Código HTTP 500 Internal Server Error
-        }
+        return $cliente;        
     }
-
 
     public function destroy($id)
     {
-        try {
-            // Busca el cliente por ID
-            $cliente = Cliente::find($id);
-    
-            if (is_null($cliente)) {
-                // Si no encuentra el cliente, devuelve un error 404
-                return response()->json([
-                    'status' => 'error',
-                    'message' => 'Cliente no encontrado.'
-                ], 404); // Código HTTP 404 Not Found
-            }
-    
-            // Elimina el cliente y devuelve un mensaje de éxito con un código 200
-            $cliente->delete();
-    
-            return response()->json([
-                'status' => 'success',
-                'message' => 'Cliente eliminado con éxito.'
-            ], 200); // Código HTTP 200 OK
-    
-        } catch (\Exception $e) {
-            // Si algo sale mal, captura la excepción y devuelve un mensaje de error con código 500
-            return response()->json([
-                'status' => 'error',
-                'message' => 'Hubo un error al eliminar el cliente.',
-                'error' => $e->getMessage()
-            ], 500); // Código HTTP 500 Internal Server Error
+        $cliente = Cliente::find($id);
+
+        if(is_null($cliente)){
+            return response()->json('No se pudo realizar correctamente la operacion', 404);
         }
+
+        $cliente->delete();
+        return response()->noContent();
     }
 }
