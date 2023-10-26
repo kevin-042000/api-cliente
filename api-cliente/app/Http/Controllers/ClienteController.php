8ed876d5da05cdcff9bfbfd946c18229bd0bdd89
Kevin Aranda <kevinaranda042000@gmail.com>
2023-10-26 10:51:38 -0300

    reformulacion de los métodos para retornar una respuesta de api estandar

--- api-cliente/app/Http/Controllers/ClienteController.php
+++ api-cliente/app/Http/Controllers/ClienteController.php
@@ -11,11 +11,21 @@ class ClienteController extends Controller
 {
     public function index()
     {
-        return Cliente::all();
+        // Obtener todos los clientes
+        $clientes = Cliente::all();
+    
+        // Devolver la respuesta en el formato solicitado
+        return response()->json([
+            'status' => 'success',  // Indica que la operación fue exitosa
+            'message' => 'Clientes obtenidos con éxito.',  // Un mensaje amigable para el usuario o el desarrollador
+            'data' => $clientes  // Los datos de todos los clientes
+        ], 200);  // Código HTTP 200 OK
     }
-
+    
     public function store(ClienteRequest $request)
-    {
+{
+    try {
+        // Crea una nueva instancia del modelo Cliente y llena los campos con datos del request
         $cliente = new Cliente;
         $cliente->name = $request->name;
         $cliente->surnames = $request->surnames;
@@ -28,42 +38,109 @@ class ClienteController extends Controller
         $cliente->city = $request->city;
         $cliente->postal_code = $request->postal_code;
 
+        // Guarda el nuevo cliente en la base de datos
         $cliente->save();
 
-        return $cliente;
+        // Devuelve la respuesta en el formato solicitado
+        return response()->json([
+            'status' => 'success',  // Indica que la operación fue exitosa
+            'message' => 'Cliente creado con éxito.',  // Un mensaje amigable para el usuario o el desarrollador
+            'data' => $cliente  // Los datos del cliente recién creado
+        ], 201);  // Código HTTP 201 Created
+
+    } catch (\Exception $e) {
+        // Si ocurre un error, captúralo y devuelve una respuesta de error
+        return response()->json([
+            'status' => 'error',
+            'message' => 'Hubo un error al crear el cliente.',
+            'error' => $e->getMessage()
+        ], 500);  // Código HTTP 500 Internal Server Error
+    }
+}
+
+
+
+
+public function show(Cliente $cliente)
+{
+    // Verifica si el cliente existe (aunque la inyección implícita de modelos de Laravel generalmente se encarga de esto)
+    if (!$cliente) {
+        return response()->json([
+            'status' => 'error',
+            'message' => 'Cliente no encontrado.',
+        ], 404);  // Código HTTP 404 Not Found
     }
 
-    public function show(Cliente $cliente)
-    {
-        return $cliente;
-    }
+    // Devuelve el cliente solicitado en el formato estándar
+    return response()->json([
+        'status' => 'success',  // Indica que la operación fue exitosa
+        'message' => 'Cliente obtenido con éxito.',  // Un mensaje amigable para el usuario o el desarrollador
+        'data' => $cliente  // Los datos del cliente solicitado
+    ], 200);  // Código HTTP 200 OK
+}
+
 
     public function update(ClienteRequest $request, Cliente $cliente)
     {
-        $cliente->name = $request->name;
-        $cliente->surnames = $request->surnames;
-        $cliente->phone_number = $request->phone_number;
-        $cliente->email = $request->email;
-        $cliente->birthdate = Carbon::createFromFormat('d/m/Y', $request->birthdate)->toDateString();
-        $cliente->address = $request->address;
-        $cliente->country = $request->country;
-        $cliente->province = $request->province;
-        $cliente->city = $request->city;
-        $cliente->postal_code = $request->postal_code;
-        $cliente->update();
-
-        return $cliente;        
+        try {
+            // Actualiza un registro
+    
+            $cliente->name = $request->name;
+            $cliente->surnames = $request->surnames;
+            $cliente->phone_number = $request->phone_number;
+            $cliente->email = $request->email;
+            $cliente->birthdate = Carbon::createFromFormat('d/m/Y', $request->birthdate)->toDateString();
+            $cliente->address = $request->address;
+            $cliente->country = $request->country;
+            $cliente->province = $request->province;
+            $cliente->city = $request->city;
+            $cliente->postal_code = $request->postal_code;
+    
+            // Guarda los datos del cliente actualizado en la base de datos
+            $cliente->update();
+    
+            // Devuelve la respuesta en el formato solicitado
+            return response()->json([
+                'status' => 'success',  // Indica que la operación fue exitosa
+                'message' => 'Cliente actualizado con éxito.',  // Un mensaje amigable para el usuario o el desarrollador
+                'data' => $cliente  // Los datos del cliente recién creado
+            ], 200);  // Código HTTP 201 Created
+    
+        } catch (\Exception $e) {
+            // Si ocurre un error, captúralo y devuelve una respuesta de error
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Hubo un error al actualizar el cliente.',
+                'error' => $e->getMessage()
+            ], 500);  // Código HTTP 500 Internal Server Error
+        }    
     }
 
     public function destroy($id)
     {
-        $cliente = Cliente::find($id);
-
-        if(is_null($cliente)){
-            return response()->json('No se pudo realizar correctamente la operacion', 404);
+        try {
+            $cliente = Cliente::find($id);
+    
+            if (is_null($cliente)) {
+                return response()->json([
+                    'status' => 'error',
+                    'message' => 'Cliente no encontrado.',
+                ], 404);  // Código HTTP 404 Not Found
+            }
+    
+            $cliente->delete();
+    
+            return response()->json([
+                'status' => 'success',
+                'message' => 'Cliente eliminado con éxito.',
+            ], 200);  // Código HTTP 200 OK
+    
+        } catch (\Exception $e) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Hubo un error al eliminar el cliente.',
+                'error' => $e->getMessage()
+            ], 500);  // Código HTTP 500 Internal Server Error
         }
-
-        $cliente->delete();
-        return response()->noContent();
     }
 }
